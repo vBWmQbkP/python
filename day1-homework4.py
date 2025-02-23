178a30d839d29c897b0e89ccfd3c2a41c1f16bfa
等风来o.o <15478262+wait-for-the-wind-to-come-o@user.noreply.gitee.com>
2025-02-24 07:09:39 +0800

    test

--- day1-homework4.py
+++ day1-homework4.py
@@ -11,5 +11,5 @@ random_ipv4=str(section1)+'.'+str(section2)+'.'+str(section3)+'.'+str(section4)
 print(random_ipv4)
 
 #way2
-random_ipv4 = ".".join(str(random.randint(1, 255)) for _ in range(4))
-print(random_ipv4)
+# random_ipv4 = ".".join(str(random.randint(1, 255)) for _ in range(4))
+# print(random_ipv4)
