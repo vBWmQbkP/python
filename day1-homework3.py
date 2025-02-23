7b6185a85bd8ed5a172b92c7322e1487acbc7a61
等风来o.o <15478262+wait-for-the-wind-to-come-o@user.noreply.gitee.com>
2025-02-23 21:43:24 +0800

    day1-homework commit

---
+++ day1-homework3.py
@@ -0,0 +1,9 @@
+#way1
+# result1 = 1 + 2
+# print("计算" + str(1) + "+" + str(2) + "的结果")
+# print("计算结果为：" + str(result1))
+
+#way2
+result2 = 1 + 2
+print(f"计算{1}+{2}的结果")
+print(f"计算结果为：{result2}")
---
+++ day1-homework4.py
@@ -0,0 +1,15 @@
+import random
+
+#way1
+section1=random.randint(1,255)
+section2=random.randint(1,255)
+section3=random.randint(1,255)
+section4=random.randint(1,255)
+
+random_ipv4=str(section1)+'.'+str(section2)+'.'+str(section3)+'.'+str(section4)
+
+print(random_ipv4)
+
+#way2
+random_ipv4 = ".".join(str(random.randint(1, 255)) for _ in range(4))
+print(random_ipv4)
