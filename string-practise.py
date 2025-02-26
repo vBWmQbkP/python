81129849cf6b3ff89aa5152348507b8fba737381
等风来o.o <15478262+wait-for-the-wind-to-come-o@user.noreply.gitee.com>
2025-02-26 23:07:16 +0800

    test

--- string-practise.py
+++ string-practise.py
@@ -33,3 +33,63 @@ str = "hello world"
 # print(str.upper())
 #
 # print(dir(str))
+
+# str0 = "hello world"
+# print(str0.upper())
+#
+# str1 = "hello world"
+# new_str1 = str1.replace("hello", "world")
+# print(new_str1)
+# print(str1)
+
+# str2 = "hello world"
+# print(str2.capitalize())
+
+# str3 = "hello world"
+# print(str3.find("world"))
+
+# str4 = "hello world"
+# print(str4.split( ))
+#
+# str5 = " \t \n \r  hello world \t \n \r  "
+# print(str5.strip(), len(str5.strip()))
+# print(str5.lstrip(), len(str5.lstrip()))
+# print(str5.rstrip(), len(str5.rstrip()))
+
+# str6 = "hello world"
+# print("A".join(str6))
+
+# list1 = ["A", "B", "C"]
+# print("".join(list1))
+
+# str7 = "hello world"
+# list2 = list(str7)
+# list2[4] = "A"
+# print(''.join(list2))
+
+# print('That is %d %s bird' % (1, 'dead'))
+# print('That is %d dead bird ' % 1)
+# print('That is 1 %s bird ' % 'dead')
+
+# str8 = 12311111111111112
+# print("%o" % str8)
+
+# str9 = 65
+# print(chr(str9))
+# print("%c" % str9)
+#
+# str10 = 2500
+# print(chr(str10))
+# print("%c" % str10)
+
+# str11 = 65
+# print("hello",repr(str11))
+# print("%r" % str11)
+
+# str12 = 123.123
+# print("%i" % str12)
+# print(int(str12))
+
+# str13 = 11111111111111111123.123
+# print("%G" % str13)
+print("The percentage is %d%%" % 10)
